01891b2a781925294bfe2e42632670b20d284a84
jaekim <jake10.kim@gmail.com>
2021-11-16 20:46:18 -0800

    tutorial_done

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    # tools for better convenience
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+#admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
@@ -10,8 +11,19 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    # applying dispaly()decorator
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        
+        # a bug
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
